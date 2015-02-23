3b4ce986a1409ca62f55c0d7ae564dddf585c792
Sandeep <it.tech.skal@gmail.com>
2015-02-23 18:23:13 +0800

    Basic main child thread example

---
+++ src/main/java/it/tech/skal/rough/thread/MyThread.java
@@ -0,0 +1,13 @@
+package it.tech.skal.rough.thread;
+
+public class MyThread extends Thread{
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			System.out.println("child thread");
+		}
+	}
+
+}
